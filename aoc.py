8e31a1adc58c00ee1b9af75eaa8914ad3dbe8ac0
sertac <sertaco@gmail.com>
2018-12-04 23:27:18 +0100

    day 3 solutions

--- aoc.py
+++ aoc.py
@@ -1,6 +1,8 @@
-from collections import Counter
-import numpy as np
 
+import numpy as np
+import datetime
+from collections import Counter
+from operator import itemgetter
 
 def d1():
 
@@ -124,6 +126,94 @@ def d3():
     print("Day 3 Part 2 result: {}".format(p2(inp)))
 
 
-if __name__ == "__main__":
-    d3()
+def d4():
 
+    def get_sleep_sheet_guards(inp):
+        with open(inp) as f:
+            records = [i.lstrip('[').split('] ') for i in f.read().strip().split('\n')]
+        records = [[datetime.datetime.strptime(i[0], "%Y-%m-%d %H:%M"), i[1]] for i in records]
+        records = sorted(records, key=itemgetter(0))
+
+        guards = []
+        sleep_sheet = dict()
+
+        for record in records:
+            if record[1].split()[0] == 'Guard':
+                guard = record[1].split()[1]
+                if guard not in guards:
+                    guards.append(guard)
+                    sleep_sheet[guard] = dict()
+            if record[1].split()[0] == 'falls':
+                if record[0].hour != 0:
+                    sleep_sheet[guard][record[0].date() + datetime.timedelta(days=1)] = np.array([1] * 60)
+                else:
+                    if record[0].date() in sleep_sheet[guard]:
+                        sleep_sheet[guard][record[0].date()][record[0].minute:] = 1
+                    else:
+                        if not record[0].date() in sleep_sheet[guard]:
+                            sleep_sheet[guard][record[0].date()] = np.array([0] * 60)
+                        sleep_sheet[guard][record[0].date()][record[0].minute:] = 1
+
+
+            elif record[1].split()[0] == 'wakes':
+
+                if record[0].hour != 0:
+                    sleep_sheet[guard][record[0].date() + datetime.timedelta(days=1)] = np.array([0] * 60)
+                else:
+                    if record[0].date() in sleep_sheet[guard]:
+                        sleep_sheet[guard][record[0].date()][record[0].minute:] = 0
+                    else:
+                        sleep_sheet[guard][record[0].date()][record[0].minute:] = 0
+
+        return sleep_sheet, guards
+
+    def p1(inp):
+        sleep_sheet, guards = get_sleep_sheet_guards(inp)
+
+        tot_sleep = []
+        for guard in guards:
+            tot = 0
+            for key in sleep_sheet[guard]:
+                tot += sum(sleep_sheet[guard][key])
+            tot_sleep.append(tot)
+
+        guard_id_most_asleep = int(guards[tot_sleep.index(max(tot_sleep))][1:])
+
+        total_slept_minutes = np.array([0]*60)
+        for key in sleep_sheet['#'+str(guard_id_most_asleep)]:
+            total_slept_minutes = np.add(total_slept_minutes, sleep_sheet['#'+str(guard_id_most_asleep)][key])
+
+        most_slept_minute = list(total_slept_minutes).index(max(total_slept_minutes))
+
+        return guard_id_most_asleep * most_slept_minute
+
+    def p2(inp):
+        sleep_sheet, guards = get_sleep_sheet_guards(inp)
+
+        most_slept_matrix = []
+        for guard in guards:
+            tot = np.array([0]*60)
+            for key in sleep_sheet[guard]:
+                tot = np.add(tot, sleep_sheet[guard][key])
+
+            most_slept_matrix.append(list(tot))
+        most_slept_matrix = np.array(most_slept_matrix)
+
+        ind = np.argmax(most_slept_matrix) // 60
+        return int(guards[ind][1:]) * (np.argmax(most_slept_matrix) % 60)
+
+    inp = "./input/4.txt"
+    print("Day 4 Part 1 result: {}".format(p1(inp)))
+    print("Day 4 Part 2 result: {}".format(p2(inp)))
+
+
+def d5():
+
+    def p1():
+        pass
+
+    def p2():
+        pass
+
+if __name__ == "__main__":
+    d4()
